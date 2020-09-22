0c2bc2863e1fe0f2cc41095291da85a5697885f9
miaoyouhu8016 <miaoyouhu8016@duia.com>
2020-09-22 18:00:09 +0800

    添加示例.

---
+++ myh-delayed-samples/src/main/java/com/myh/delayed/DelayedApplication.java
@@ -0,0 +1,13 @@
+package com.myh.delayed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelayedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelayedApplication.class, args);
+	}
+
+}
